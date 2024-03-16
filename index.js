52cd8f4c3115bcdf15c0b895a5d5e7758e696a81
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-03-16 17:19:56 +0600

    create api to get all users

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function run() {
     const cartCollection = client.db("BistroDB").collection("carts");
 
     // user related api
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
 
