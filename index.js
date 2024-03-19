64ae89b38ec7da24dfd7d086250fd3b5355ba830
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-03-19 23:45:46 +0600

    added new api to get admin role

--- index.js
+++ index.js
@@ -45,14 +45,18 @@ async function run() {
 
     // middleware
     const verifyToken = (req, res, next) => {
-      console.log("inside verify token", req.header);
+      console.log("inside verify token", req.headers.authorization);
       if (!req.headers.authorization) {
         return res.status(401).send({ message: "forbidden access" });
       }
-      const token = req.headers.authorization.split("")[1];
-      if (!token) {
-      }
-      // next();
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "forbiden access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
     };
 
     // user related api
@@ -60,6 +64,21 @@ async function run() {
       console.log(req.headers);
       const result = await userCollection.find().toArray();
       res.send(result);
+    });
+
+    app.get("/user/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "unauthorized access" });
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
     });
 
     app.post("/users", async (req, res) => {
