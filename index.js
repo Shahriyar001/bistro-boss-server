a68542da889691ea585be290c3bc6e476c9dd7f2
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-03-23 23:23:18 +0600

    added delete api method for mrnu item

--- index.js
+++ index.js
@@ -142,6 +142,14 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
